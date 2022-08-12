a20350fc63e5229f4a2a95b1162715d78298e4fe
Georg Schneider <georg.schneider@gmail.com>
2022-08-12 08:17:54 +0200

    Add remaining bond instruments (#14)

--- ui/src/context/InstrumentsContext.tsx
+++ ui/src/context/InstrumentsContext.tsx
@@ -7,12 +7,18 @@ import { useStreamQueries } from "@daml/react";
 import { Instrument as Token } from "@daml.js/daml-finance-asset/lib/Daml/Finance/Asset/Instrument";
 import { Instrument as Derivative } from "@daml.js/daml-finance-derivative/lib/Daml/Finance/Derivative/Instrument";
 import { FixedRateBond } from "@daml.js/daml-finance-bond/lib/Daml/Finance/Bond/FixedRate";
+import { FloatingRateBond } from "@daml.js/daml-finance-bond/lib/Daml/Finance/Bond/FloatingRate";
+import { InflationLinkedBond } from "@daml.js/daml-finance-bond/lib/Daml/Finance/Bond/InflationLinked";
+import { ZeroCouponBond } from "@daml.js/daml-finance-bond/lib/Daml/Finance/Bond/ZeroCoupon";
 
 export type InstrumentsState = {
   loading : boolean
   tokens : readonly CreateEvent<Token>[]
   derivatives : readonly CreateEvent<Derivative>[]
   fixedRateBonds : readonly CreateEvent<FixedRateBond>[]
+  floatingRateBonds : readonly CreateEvent<FloatingRateBond>[]
+  inflationLinkedBonds : readonly CreateEvent<InflationLinkedBond>[]
+  zeroCouponBonds : readonly CreateEvent<ZeroCouponBond>[]
 };
 
 const empty = {
@@ -20,6 +26,9 @@ const empty = {
   tokens: [],
   derivatives: [],
   fixedRateBonds: [],
+  floatingRateBonds: [],
+  inflationLinkedBonds: [],
+  zeroCouponBonds: [],
 };
 
 const InstrumentsContext = React.createContext<InstrumentsState>(empty);
@@ -29,13 +38,19 @@ export const InstrumentsProvider : React.FC = ({ children }) => {
   const { contracts: tokens, loading: l1 } = useStreamQueries(Token);
   const { contracts: derivatives, loading: l2 } = useStreamQueries(Derivative);
   const { contracts: fixedRateBonds, loading: l3 } = useStreamQueries(FixedRateBond);
-  const loading = l1 || l2 || l3;
+  const { contracts: floatingRateBonds, loading: l4 } = useStreamQueries(FloatingRateBond);
+  const { contracts: inflationLinkedBonds, loading: l5 } = useStreamQueries(InflationLinkedBond);
+  const { contracts: zeroCouponBonds, loading: l6 } = useStreamQueries(ZeroCouponBond);
+  const loading = l1 || l2 || l3 || l4 || l5 || l6;
 
   const value = {
     loading,
     tokens,
     derivatives,
     fixedRateBonds,
+    floatingRateBonds,
+    inflationLinkedBonds,
+    zeroCouponBonds,
   };
 
   return (
