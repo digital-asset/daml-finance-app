0b573d09970bd1ed7831dccc91d178cdf4115f3b
Georg Schneider <georg.schneider@gmail.com>
2022-09-14 00:56:13 +0200

    Add bond issuance scenario

--- ui/src/context/ScenarioContext.tsx
+++ ui/src/context/ScenarioContext.tsx
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React, { useState } from "react";
+import bondIssuanceImage from "../images/bondIssuance.png";
+import defaultImage from "../images/defaultScenario.png";
+import structuredNotesImage from "../images/structuredNotesScenario.png";
+import naturalGasImage from "../images/naturalGasScenario.png";
 
 type Position = {
   x : number
@@ -9,7 +13,9 @@ type Position = {
 };
 
 export type Scenario = {
-  name : string,
+  label : string,
+  description : string,
+  image : string,
   positions : Map<string, Position>,
   useNetworkLogin: boolean
 };
@@ -21,7 +27,9 @@ export type ScenarioState = {
 
 export const scenarios : Scenario[] = [
   {
-    name: "Default",
+    label: "Default",
+    description: "Primary and secondary markets workflows",
+    image: defaultImage,
     positions: new Map([
       [ "Operator",     { x:    0, y:   0 } ],
       [ "Public",       { x:    0, y:   0 } ],
@@ -37,7 +45,39 @@ export const scenarios : Scenario[] = [
     useNetworkLogin: true
   },
   {
-    name: "Structured Notes",
+    label: "Bond Issuance",
+    description: "Simple bond issuance custody scenario",
+    image: bondIssuanceImage,
+    positions: new Map([
+      [ "Operator",     { x:    0, y:   0 } ],
+      [ "Public",       { x:    0, y:   0 } ],
+      [ "CentralBank",  { x:  400, y:   0 } ],
+      [ "Registry",     { x:  800, y:   0 } ],
+      [ "BNY",          { x:  400, y: 300 } ],
+      [ "Issuer",       { x:  800, y: 300 } ],
+      [ "Alice",        { x:    0, y: 600 } ],
+      [ "Bob",          { x:  400, y: 600 } ],
+      [ "Charlie",      { x:  800, y: 600 } ]
+    ]),
+    useNetworkLogin: true
+  },
+  {
+    label: "Natural Gas",
+    description: "Modeling complex commodity trades",
+    image: naturalGasImage,
+    positions: new Map([
+      [ "Operator",     { x:    0, y:   0 } ],
+      [ "Public",       { x:    0, y:   0 } ],
+      [ "CentralBank",  { x:  200, y:   0 } ],
+      [ "Seller",       { x:  400, y: 200 } ],
+      [ "Buyer",        { x:    0, y: 400 } ]
+    ]),
+    useNetworkLogin: true
+  },
+  {
+    label: "Structured Notes",
+    description: "Synchronized issuance for structured products",
+    image: structuredNotesImage,
     positions: new Map([
       [ "Operator",     { x:    0, y:   0 } ],
       [ "Public",       { x:    0, y:   0 } ],
@@ -50,17 +90,6 @@ export const scenarios : Scenario[] = [
       [ "Charlie",      { x:  600, y: 450 } ]
     ]),
     useNetworkLogin: true
-  },
-  {
-    name: "Natural Gas",
-    positions: new Map([
-      [ "Operator",     { x:    0, y:   0 } ],
-      [ "Public",       { x:    0, y:   0 } ],
-      [ "CentralBank",  { x:  200, y:   0 } ],
-      [ "Seller",       { x:  400, y: 200 } ],
-      [ "Buyer",        { x:    0, y: 400 } ]
-    ]),
-    useNetworkLogin: true
   }
 ];
 
@@ -68,13 +97,13 @@ const ScenarioContext = React.createContext<ScenarioState>({ selected: scenarios
 
 export const ScenarioProvider : React.FC = ({ children }) => {
   const scenarioName = localStorage.getItem("daml.scenario") || "Default";
-  const scenario = scenarios.find(s => s.name === scenarioName);
+  const scenario = scenarios.find(s => s.label === scenarioName);
   if (!scenario) throw new Error("Couldn't find scenario " + scenarioName);
 
   const [ selected, setSelected ] = useState(scenario);
 
   const select = (name : string) : Scenario => {
-    const s = scenarios.find(s => s.name === name);
+    const s = scenarios.find(s => s.label === name);
     if (!s) throw new Error("Couldn't find scenario " + name);
     localStorage.setItem("daml.scenario", name);
     setSelected(s);
--- ui/src/pages/login/Portal.tsx
+++ ui/src/pages/login/Portal.tsx
@@ -6,10 +6,8 @@ import { Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { Scenario } from "../../components/Card/Scenario";
 import damlLogin from "../../images/daml-logo-mark-light.png";
-import defaultImage from "../../images/defaultScenario.png";
-import structuredNotesImage from "../../images/structuredNotesScenario.png";
-import naturalGasImage from "../../images/naturalGasScenario.png";
 import useStyles from "./styles";
+import { scenarios } from "../../context/ScenarioContext";
 
 export const Portal : React.FC = () => {
   const classes = useStyles();
@@ -17,14 +15,12 @@ export const Portal : React.FC = () => {
   return (
     <>
       <img alt="loginLogo" src={damlLogin} style={{ position: "absolute", top: "10%", left: "50%", transform: "translate(-50%, 0%)", display: "inline-block", zIndex: 0 }} />
-      <Typography variant="h2" style={{ textAlign: "center" }}>Digital Market Infrastructure</Typography>
+      <Typography variant="h2" style={{ textAlign: "center" }}>Daml Finance Reference Application</Typography>
       <Box className={classes.loginContainer} style={{ position: "absolute", top: "30%", left: "50%", transform: "translate(-50%, 0%)", width: "1600px", height: "600px", backgroundColor: "#f5f5f5" }}>
         <Grid container direction="column">
           <Grid item xs={12}>
             <Grid container direction="row" spacing={4}>
-              <Scenario label="Default"           description="Default workflows"                 image={defaultImage}          width={4} />
-              <Scenario label="Structured Notes"  description="Issuing structured products"       image={structuredNotesImage}  width={4} />
-              <Scenario label="Natural Gas"       description="Modeling complex commodity trades" image={naturalGasImage}       width={4} />
+              {scenarios.map(s => (<Scenario {...s} width={6} />))}
             </Grid>
           </Grid>
         </Grid>
