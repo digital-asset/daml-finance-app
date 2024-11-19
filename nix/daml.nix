7bc1e0761d43c750a1d68c6f68a9914a18902c4d
Marcin Ziolek <marcin.ziolek@digitalasset.com>
2024-11-19 15:07:07 +0100

    Compile project under 2.10

--- nix/daml.nix
+++ nix/daml.nix
@@ -1,20 +1,77 @@
-{ stdenv, jdk, version }:
+{ stdenv, jdk, curl, curl_cert, sdkVersion, damlVersion, tarPath, os, osJFrog, hashes }:
 let
-  os = if stdenv.isDarwin then "macos" else "linux";
-  tarball = fetchTarball {
-    url = "https://github.com/digital-asset/daml/releases/download/v${version}/daml-sdk-${version}-${os}.tar.gz";
+  nixTarPath = if tarPath == null then null else /. + tarPath;
+  tarball = stdenv.mkDerivation {
+    pname = "daml-tarball";
+    version = sdkVersion;
+    src = ../.;
+    buildInputs = [ curl ];
+    SSL_CERT_FILE = curl_cert;
+    impureEnvVars = [ "ARTIFACTORY_USERNAME" "ARTIFACTORY_PASSWORD" ];
+    buildPhase = ''
+      set -euo pipefail
+
+      if [ -f .envrc.private ]; then
+        source .envrc.private
+      fi
+
+      get_local() (
+        tar_path="${if nixTarPath == null then "" else nixTarPath}"
+        if [ -z $tar_path ]; then
+          echo "No explict tar set, attempting to download"
+          exit 1
+        else
+          echo "Using explicit tar"
+          cp $tar_path $out
+          chmod -x $out
+        fi
+      )
+      get_os() (
+        echo "Downloading SDK from GitHub..."
+        curl --location \
+             --fail \
+             https://github.com/digital-asset/daml/releases/download/v${sdkVersion}/daml-sdk-${damlVersion}-${os}.tar.gz \
+          > $out
+      )
+      get_ee() (
+        echo "Downloading SDK from Artifactory..."
+        if [ -n "''${ARTIFACTORY_PASSWORD:-}" ]; then
+          curl -u $ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD \
+               https://digitalasset.jfrog.io/artifactory/assembly/daml/${sdkVersion}/daml-sdk-${sdkVersion}-${osJFrog}.tar.gz \
+            > $out
+        else
+          echo "ARTIFACTORY_USERNAME and ARTIFACTORY_PASSWORD must be set." >&2
+          exit 1
+        fi
+      )
+
+      get_local || get_os || get_ee
+    '';
+    dontInstall = true;
+    outputHashAlgo = "sha256";
+    outputHashMode = "flat";
+    outputHash = hashes.${os};
   };
 in
   stdenv.mkDerivation {
-    name = "daml-sdk";
-    version = "$version";
+    pname = "daml-sdk";
+    version = sdkVersion;
     src = tarball;
-    buildPhase = "patchShebangs .";
-    installPhase = "DAML_HOME=$out ./install.sh";
+    dontUnpack = true;
+    buildPhase = ''
+      mkdir daml
+      tar xzf $src -C daml --strip-components 1
+      patchShebangs .
+    '';
+    installPhase = ''
+      cd daml
+      export DAML_HOME=$out
+      ./daml/daml install --install-assistant yes --install-with-custom-version $version $src
+    '';
     propagatedBuildInputs = [ jdk ];
     preFixup = ''
       # Set DAML_HOME automatically.
       mkdir -p $out/nix-support
       echo export DAML_HOME=$out > $out/nix-support/setup-hook
     '';
-}
+  }
--- shell.nix
+++ shell.nix
@@ -4,17 +4,26 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
-  daml = import ./nix/daml.nix;
+  build_daml = import ./nix/daml.nix;
   damlYaml = builtins.fromJSON (builtins.readFile (pkgs.runCommand "daml.yaml.json" { yamlFile = ./daml.yaml; } ''
                 ${pkgs.yj}/bin/yj < "$yamlFile" > $out
               ''));
+  daml = (build_daml { stdenv = pkgs.stdenv;
+                       jdk = pkgs.openjdk11_headless;
+                       sdkVersion = damlYaml.sdk-version;
+                       damlVersion = damlYaml.daml-version;
+                       tarPath = damlYaml.daml-tar-path or null;
+                       curl = pkgs.curl;
+                       curl_cert = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+                       os = if pkgs.stdenv.isDarwin then "macos" else "linux";
+                       osJFrog = if pkgs.stdenv.isDarwin then "macos" else "linux-intel";
+                       hashes = { linux = "SM26LFj43g4TFba1EpLFazaEdvDFOufvMPrbGPYYP0o=";
+                                  macos = "wdXq5PVge6pyf3FuW+winRhMM7707Lg0b4cr4L8+nE4="; };});            
 in
 pkgs.mkShell {
   SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
   buildInputs = with pkgs; [
-    (daml { stdenv = stdenv;
-            jdk = openjdk11_headless;
-            version = damlYaml.sdk-version; })
+    daml
     bash
     binutils
     cacert
