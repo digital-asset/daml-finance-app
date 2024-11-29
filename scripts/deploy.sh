8c3abf21a9e963cbf8dc189cedb692dc5af36cce
Marcin Ziolek <marcin.ziolek@digitalasset.com>
2024-11-29 11:19:25 +0100

    multi-package setup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@
 # Use absolute paths to allow this script to be called from any location
 script_dir=$(cd "$(dirname $0)"; pwd -P)
 root_dir=$(cd ${script_dir}; cd ..; pwd -P)
+setup_dir=${root_dir}/package/main/daml/Daml.Finance.Setup
 
 clean () {
     if [[ -d ${root_dir}/.dars ]]; then
@@ -13,9 +14,7 @@ clean () {
 }
 
 build () {
-    DAML_PROJECT=${root_dir}/package/main/daml/Daml.Finance.Interface daml build
-    DAML_PROJECT=${root_dir}/package/main/daml/Daml.Finance.App daml build
-    DAML_PROJECT=${root_dir}/package/main/daml/Daml.Finance.Setup daml build
+    daml build --all
 
     # Copy all package dars into a dedicated folder
     mkdir -p ${root_dir}/.dars
@@ -27,8 +26,10 @@ start () {
     mkdir -p ${root_dir}/temp
     output_file=$(mktemp -p ${root_dir}/temp)
 
+    pushd ${setup_dir} > /dev/null || exit
     daml sandbox > ${output_file} & 
     sandbox_pid=$!
+    popd > /dev/null || exit
 
     echo "Sandbox running with pid: ${sandbox_pid}"
     tail -n0 -f ${output_file} | sed '/Canton sandbox is ready/ q'
@@ -39,23 +40,33 @@ start () {
 }
 
 dars() {
-    find ${root_dir}/.dars/ -name "*${1}.dar" -exec daml ledger upload-dar --host localhost --port 6865 {} \;
+    find ${root_dir}/package/main/daml/ -name "*${1}.dar" -exec daml ledger upload-dar --host localhost --port 6865 {} \;
 }
 
 setup () {
+    pushd ${setup_dir} > /dev/null || exit
     script_name=$(yq e '.init-script' daml.yaml)
     script_options=$(yq e '.script-options[]' daml.yaml | tr '\n' ' ')
-    dar_name="${root_dir}/.dars/daml-finance-app-setup-${1}.dar"
+    dar_name="./.daml/dist/daml-finance-app-setup-${1}.dar"
     if [ ! -f ${dar_name} ]; then
         echo "Setup dar not found!"
     fi
     echo "Running the script: ${script_name}"
     daml script --dar ${dar_name} --script-name ${script_name} --ledger-host localhost --ledger-port 6865 ${script_options}
+    popd > /dev/null || exit
 }
 
 json() {
+    pushd ${setup_dir} > /dev/null || exit
     json_options=$(yq e '.json-api-options[]' daml.yaml | tr '\n' ' ')
-    daml json-api --allow-insecure-tokens ${json_options} 
+    daml json-api --allow-insecure-tokens ${json_options}
+    popd > /dev/null || exit
+}
+
+codegen() {
+    pushd ${setup_dir} > /dev/null || exit
+    daml codegen js
+    popd > /dev/null || exit
 }
 
 stop () {
@@ -90,6 +101,10 @@ case $operation in
 
   json)
     json
+    ;;
+
+  codegen)
+    codegen
     ;;
 
 esac
